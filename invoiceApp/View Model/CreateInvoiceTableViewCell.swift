23ba722f8c6b7214ac23ac9a35fec336ce11c580
h-93 <hanif234@hotmail.com>
2023-12-27 21:26:16 +0000

    modified db and invoice layout

---
+++ invoiceApp/View Model/CreateInvoiceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CreateInvoiceTableViewCell.swift
+//  invoiceApp
+//
+//  Created by hanif hussain on 27/12/2023.
+//
+
+import UIKit
+
+class CreateInvoiceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
