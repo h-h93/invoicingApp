efcbeccd5ccf77cd3bb5c71f835adc49955b38a0
h-93 <hanif234@hotmail.com>
2024-01-06 23:55:18 +0000

    streamlined code by removing repetative db functions into a single dbOperations class need to add filtering results in the databaseOperations class.

---
+++ invoiceApp/Model/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  invoiceApp
+//
+//  Created by hanif hussain on 06/01/2024.
+//
+
+import Foundation
---
+++ invoiceApp/previous.swift
@@ -0,0 +1,8 @@
+//
+//  previous.swift
+//  invoiceApp
+//
+//  Created by hanif hussain on 06/01/2024.
+//
+
+import Foundation
