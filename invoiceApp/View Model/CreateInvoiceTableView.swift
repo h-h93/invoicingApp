ef188916c150ff29c54f992e90941bd6b3473b23
h-93 <hanif234@hotmail.com>
2024-01-09 00:42:33 +0000

    created a custom uiview called CreateInvoiceTableView to streamline codebase and stick to oop and MVVM principles

---
+++ invoiceApp/View Model/CreateInvoiceTableView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateInvoiceTableView.swift
+//  invoiceApp
+//
+//  Created by hanif hussain on 08/01/2024.
+//
+
+import UIKit
+
+class CreateInvoiceTableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
